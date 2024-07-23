51ad74f9b12431f76d0a4e15154e020c758252bc
shcho0123 <jsh980123@gmail.com>
2024-07-23 16:02:40 +0900

    임시용

---
+++ hikingclub/src/main/java/com/gam/signup/SignupApplication.java
@@ -0,0 +1,10 @@
+package com.gam.signup;
+
+import org.springframework.boot.SpringApplication;
+
+public class SignupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignupApplication.class, args);
+    }
+}
